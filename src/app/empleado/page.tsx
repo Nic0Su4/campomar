5797f86c3d2355d3c02015786d19071d0aa1e8ec
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2024-08-23 21:56:37 -0500

    agregado controlador de estado zustand

--- src/app/empleado/page.tsx
+++ src/app/empleado/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { useEmpleadoStore } from "@/store/empleado";
+import { empleados } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 const EmpleadoPage = () => {
-  const empleado = useEmpleadoStore((state: any) => state.empleado);
+  const empleado = useEmpleadoStore(
+    (state: any) => state.empleado
+  ) as empleados | null;
 
   if (!empleado) {
     redirect("/login");
   }
 
-  return <div>empleado page</div>;
+  return <div>hola {empleado.Nombre}</div>;
 };
 
 export default EmpleadoPage;
